90c6cf1620b38b37558663ecb02c446124e90259
Michael Gleicher <gleicher@cs.wisc.edu>
2021-08-10 21:34:45 -0500

    check against EEBO metadata and write results to file

--- dircheck.py
+++ dircheck.py
@@ -21,6 +21,7 @@ play_dir = "C:\\DigHum\\Plays\\emd1554_charCleanExtractedSt"
 # the metada file to check
 tcp_meta = "https://raw.githubusercontent.com/uwgraphics/VEP-corpora/master/tcp_ascii_standardized.csv"
 drama_meta = "https://raw.githubusercontent.com/uwgraphics/VEP-corpora/master/vep_early_modern_drama_all_known_texts_master.csv"
+eebo_meta = "https://raw.githubusercontent.com/uwgraphics/VEP-corpora/master/VEP2/tls-eebo-2021-plus.csv"
 
 # get the list of files
 def getPaths(dir, ext):
@@ -32,7 +33,8 @@ c_tn = []
 c_fn = []
 
 # get a list of files, the meta data, and then match...
-def check(*, dir=play_dir, ext=".txt", meta=drama_meta, downcase=False, suffix=False, rows_to_show=5):
+def check(*, dir=play_dir, ext=".txt", meta=drama_meta, downcase=False, suffix=False,
+          rows_to_show=5, writefile=False):
     global c_tn,c_fn
 
     # get full paths
@@ -51,7 +53,7 @@ def check(*, dir=play_dir, ext=".txt", meta=drama_meta, downcase=False, suffix=F
 
     # fetch metadata
     print("Fetching meta data")
-    meta = pandas.read_csv(meta).fillna(False)
+    meta = pandas.read_csv(meta,dtype=str).fillna(False)
 
     # create a list of the file names...
     # the drama metadata has this - the TCP metadata, maybe not...
@@ -59,7 +61,7 @@ def check(*, dir=play_dir, ext=".txt", meta=drama_meta, downcase=False, suffix=F
     if "text_name" in meta.columns:
         textnames = [os.path.splitext(t)[0] if t else False for t in meta["text_name"]]
     elif "TCP" in meta.columns:
-        textnames = [ t+("" if t[0]=="K" else ".headed") for t in meta["TCP"]]
+        textnames = [ (t+("" if t[0]=="K" else ".headed")) if t else "" for t in meta["TCP"]]
 
     if downcase:
         textnames = [t.lower() if t else False for t in textnames]
@@ -101,6 +103,17 @@ def check(*, dir=play_dir, ext=".txt", meta=drama_meta, downcase=False, suffix=F
         for ri in rows_without[:rows_to_show]:
             print("   Row {}: '{}'".format(ri,textnames[ri]))
 
+    if writefile:
+        print("Writing Report",writefile)
+        with open(writefile,"w") as fo:
+            fo.write("Files without rows ({} of {})\n".format(len(not_in_meta),len(files)))
+            for fi in not_in_meta:
+                fo.write("   {}\n".format(fi))
+            fo.write("-------------------------------------------------------\n")
+            fo.write("Rows without files ({} of {}):\n".format(len(rows_without),len(textnames)))
+            for ri in rows_without:
+                fo.write("   Row {}: '{}'\n".format(ri, textnames[ri]))
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Check that a directory has a full set of files")
     parser.add_argument("directory",type=str,help="Path to the directory with files (it does recurse)")
@@ -109,15 +122,17 @@ if __name__ == '__main__':
     parser.add_argument("-d","--downcase",help="Convert names (both sides) to lower case",action="store_true")
     parser.add_argument("-s","--suffix",type=str,help="Suffix to add to base names (use = for leading dash)",default="")
     parser.add_argument("-r","--rows_to_show",type=int,help="Number of missing rows to show",default=5)
+    parser.add_argument("-w","--writefile",type=str,default=False,help="write report file")
     args = parser.parse_args()
     repo = None
     if not(args.repo) or args.repo[0]=="T" or args.repo[0]=="t":
         repo = tcp_meta
         print("using TCP repository")
+    elif args.repo[0]=="E" or args.repo[0]=="e":
+        repo = eebo_meta
     elif args.repo[0]=="D" or args.repo[0]=="d":
         repo = drama_meta
         print("using Drama repository")
-    print("Downcase",args.downcase)
     check(dir=args.directory,ext=args.ext,meta=repo,downcase=args.downcase,
-          suffix=args.suffix,rows_to_show=args.rows_to_show)
+          suffix=args.suffix,rows_to_show=args.rows_to_show,writefile=args.writefile)
 
